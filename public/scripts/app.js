d6e9efdefbb6f65b6d9791f1e6efde39a08f2a05
billhimmelsbach <whimmels@gmail.com>
2016-07-23 12:29:49 -0700

    WE HAVE MARKER RENDERING. Moves resultsMap.setCenter back up into the correct scope. Javascript currently in index for testing.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,49 +2,10 @@ console.log("Sanity Check: JS is working!");
 var template;
 var $clinicsList;
 var allClinics = [];
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
-    center: {lat: -34.397, lng: 150.644}
-  });
-  var geocoder = new google.maps.Geocoder();
-  document.getElementById('submit').addEventListener('click', function() {
-    geocodeAddress(geocoder, map);
-  });
-}
-
-function geocodeAddress(geocoder, resultsMap) {
-  var loc=[];
-  var address = document.getElementById('address').value;
-  geocoder.geocode({'address': address}, function(results, status) {
-    if (status === google.maps.GeocoderStatus.OK) {
-    loc[0]=results[0].geometry.location.lat();
-    loc[1]=results[0].geometry.location.lng();
-    console.log( loc ); // the place where loc contains geocoded coordinates
-    $.get('/api/locations?longitude=' + loc[0] +'&latitude='+loc[1]).success(function (clinicsNearby) {
-      console.log(clinicsNearby);
-      resultsMap.setCenter(results[0].geometry.location);
-      $.each(clinicsNearby, function(key, data) {
-        var latLng = new google.maps.LatLng(data.loc[0], data.loc[1]);
-        var marker = new google.maps.Marker({
-          position: latLng,
-          title: data.name,
-        });
-        marker.setMap(resultsMap);
-      });
-      var marker = new google.maps.Marker({
-      map: resultsMap,
-      position: results[0].geometry.location
-      });
-    });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
 
 $(document).ready(function(){
 $('.modal-trigger').leanModal();
+
   $('.modal-test').on('click', function(e) {
       console.log('add-clinic clicked!');
 
