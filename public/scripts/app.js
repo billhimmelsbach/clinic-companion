9e7412e69ff5cdfcc4c3b447e70222005b638a31
billhimmelsbach <whimmels@gmail.com>
2016-07-23 07:14:52 -0700

    google terms and contitions prevents use of geocoder without map, so imported code from previous personal-api project to push the map off the screen

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,9 +2,37 @@ console.log("Sanity Check: JS is working!");
 var template;
 var $clinicsList;
 var allClinics = [];
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: {lat: -34.397, lng: 150.644}
+  });
+  var geocoder = new google.maps.Geocoder();
+}
 
+function geocodeAddress(geocoder, resultsMap) {
+  var loc=[];
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+    loc[0]=results[0].geometry.location.lat();
+    loc[1]=results[0].geometry.location.lng();
+    console.log( loc ); // the place where loc contains geocoded coordinates
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
 
 $(document).ready(function(){
+  document.getElementById('submit').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+  });
 $('.modal-trigger').leanModal();
   $('.modal-test').on('click', function(e) {
       console.log('add-clinic clicked!');
