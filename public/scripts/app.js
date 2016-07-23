d824a0300ac572eac09eb61a0a16df093d547a16
billhimmelsbach <whimmels@gmail.com>
2016-07-23 06:59:10 -0700

    adds test Google Map that console.logs the lat, lng of a geolocated object

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,26 +3,8 @@ var template;
 var $clinicsList;
 var allClinics = [];
 
+
 $(document).ready(function(){
-  $(function() {
-
-    $('#login-form-link').click(function(e) {
-		$("#login-form").delay(100).fadeIn(100);
- 		$("#register-form").fadeOut(100);
-		$('#register-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-	$('#register-form-link').click(function(e) {
-		$("#register-form").delay(100).fadeIn(100);
- 		$("#login-form").fadeOut(100);
-		$('#login-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-
-});
-
 $('.modal-trigger').leanModal();
   $('.modal-test').on('click', function(e) {
       console.log('add-clinic clicked!');
