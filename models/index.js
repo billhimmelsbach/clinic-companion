a33d7343ce027916685987c95050e927041e8437
billhimmelsbach <whimmels@gmail.com>
2016-07-25 02:36:05 -0700

    preparing for heroku deployment

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/clinic-companion");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/personal-api");
 
 module.exports.Clinic = require("./clinic.js");
 module.exports.Location = require("./location.js");
